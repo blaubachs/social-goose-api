70fffd66de485cf3037c7f0e66afce75cbfd9908
Ben L <veloswim@gmail.com>
2023-02-23 17:23:11 -0800

    added virtuals toJSON option to reaction schema

--- models/User.js
+++ models/User.js
@@ -5,12 +5,19 @@ function checkEmail(email) {
   return regexEmailChecker.test(email);
 }
 
-const reactionSchema = new mongoose.Schema({
-  reactionId: { type: ObjectId, default: new ObjectId() },
-  reactionBody: { type: String, required: true, maxLength: 280 },
-  username: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: { type: ObjectId, default: new ObjectId() },
+    reactionBody: { type: String, required: true, maxLength: 280 },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 const thoughtSchema = new mongoose.Schema(
   {
