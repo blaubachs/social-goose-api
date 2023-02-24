30345eb00c2e8ebe23e437a1406aebf13bfc676b
Ben L <veloswim@gmail.com>
2023-02-23 18:11:26 -0800

    seeds now work for users

--- seeds/index.js
+++ seeds/index.js
@@ -1,2 +1,29 @@
 // run seeds here, will have to seed users and their respective thoughts
 const connection = require("../config/connection");
+const { User, Thoughts } = require("../models");
+
+connection.once("open", async () => {
+  // Delete all data
+  await User.deleteMany({});
+  await Thoughts.deleteMany({});
+  // Seed data...
+
+  let userData = [
+    {
+      username: "bingus",
+      email: "bingus@bingus.gov",
+    },
+    {
+      username: "notBingus",
+      email: "email@email.com",
+    },
+    {
+      username: "yahoo",
+      email: "yahoo@yahoo.yahoo",
+    },
+  ];
+
+  await User.collection.insertMany(userData);
+
+  console.log("Seeded  data into users");
+});
